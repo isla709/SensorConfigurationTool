c3d69cdb306a461ec55284690d82671c2d90d91b
Noir <hjs625709@outlook.com>
2026-01-06 16:47:38 +0800

    V2601061547

--- Soway传感器配置工具/MainWindow.xaml.cs
+++ Soway传感器配置工具/MainWindow.xaml.cs
@@ -19,13 +19,17 @@ namespace Soway传感器配置工具
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> SysPortList = [];
-        List<string> BaudList = ["4800","9600","38400","115200"];
-        SerialPort? CommPort = null;
+        internal List<string> SysPortList = [];
+        internal List<string> BaudList = ["2400","4800","9600", "19200", "38400", "57600", "115200"];
+        internal SerialPort? CommPort = null;
 
-        string MBData_Head = ":";
-        string[] MBData_Addrs = ["41", "42", "43", "44"];
-        string[] MBData_Functions = ["03", "10", "05", "0F", "04", "2B"];
+        internal string MBData_Head = ":";
+        internal string[] MBData_Addrs = ["41", "42", "43", "44"];
+        internal string[] MBData_Functions = ["03", "10", "05", "0F", "04", "2B"];
+
+        internal string currentDeviceAddr = "N/D";
+
+        internal List<ushort> DeviceADValue = new List<ushort>();
 
         public MainWindow()
         {
@@ -65,6 +69,22 @@ namespace Soway传感器配置工具
 
             btn_PortSwitch.Tag = false;
 
+            Task.Run(async () => {
+                TextBox? textBox = cc_Deviceaddr.Template.FindName("LT_Textbox", cc_Deviceaddr) as TextBox;
+                if (textBox == null) {
+                    MessageBox.Show("控件cc_Deviceaddr为Null","未知错误",MessageBoxButton.OK,MessageBoxImage.Error);
+                    return;
+                }
+                while (true)
+                {
+                    await Task.Delay(1000);
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        textBox.Text = currentDeviceAddr;
+                    });
+                }
+            });
+
         }
 
         private void btn_reloadports_Click(object sender, RoutedEventArgs e)
@@ -77,12 +97,42 @@ namespace Soway传感器配置工具
 
         private void btn_PortSwitch_Click(object sender, RoutedEventArgs e)
         {
+
             ComboBox? cb_port = cc_Port.Template.FindName("LC_Combobox", cc_Port) as ComboBox;
             ComboBox? cb_baud = cc_Baud.Template.FindName("LC_Combobox", cc_Baud) as ComboBox;
+            try
+            {
+
+                if (cb_port == null)
+                {
+                    throw new Exception("控件cb_port为空");
+                }
+                if (cb_baud == null)
+                {
+                    throw new Exception("控件cb_baud为空");
+
+                }
+                if (cb_port.SelectedIndex == -1 || cb_baud.SelectedIndex == -1)
+                {
+                    MessageBox.Show("请选择串口和波特率");
+                    return;
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("打开串口失败:" + ex.Message);
+                return;
+            }
+
+
+
 
             if (btn_PortSwitch.Tag as bool? == false)
             {
                 btn_PortSwitch.Tag = true;
+                btn_reloadports.IsEnabled = false;
+                cb_port!.IsEnabled = false;
+                cb_baud!.IsEnabled = false;
 
                 CommPort = new SerialPort();
                 CommPort.PortName = cb_port!.SelectedValue.ToString();
@@ -94,7 +144,11 @@ namespace Soway传感器配置工具
             else
             {
                 btn_PortSwitch.Tag = false;
-                if(CommPort!.IsOpen)
+                btn_reloadports.IsEnabled = true;
+                cb_port!.IsEnabled = true;
+                cb_baud!.IsEnabled = true;
+
+                if (CommPort!.IsOpen)
                 {
                     CommPort.DataReceived -= CommPort_DataReceived;
                     CommPort.Close();
@@ -116,7 +170,7 @@ namespace Soway传感器配置工具
 
         }
 
-        List<byte> RecvPortData = new List<byte>();
+        internal List<byte> RecvPortData = new List<byte>();
         private void CommPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             int DataSize = CommPort.BytesToRead;
@@ -126,7 +180,7 @@ namespace Soway传感器配置工具
             waitPortRecvSignal.Set();
         }
 
-        ManualResetEventSlim waitPortRecvSignal = new ManualResetEventSlim(false);
+        internal ManualResetEventSlim waitPortRecvSignal = new ManualResetEventSlim(false);
         private async void btn_FindDevice_Click(object sender, RoutedEventArgs e)
         {
             if (CommPort == null) {
@@ -135,7 +189,8 @@ namespace Soway传感器配置工具
             }
             if (CommPort?.IsOpen == true) 
             {
-                
+                btn_FindDevice.IsEnabled = false;
+
                 for (int i = 0; i < MBData_Addrs.Length; i++)
                 {
                     string FindDeviceCmd = MBData_Addrs[i];
@@ -144,22 +199,37 @@ namespace Soway传感器配置工具
                     FindDeviceCmd += "30";
                     FindDeviceCmd += "0001";
                     FindDeviceCmd = ModbusAsciiLrcHelper.GenerateCompleteAsciiCmdWithLrc(FindDeviceCmd);
+                    Trace.WriteLine("----------------------------------------------------------------------------");
                     Trace.WriteLine("尝试发送指令:" + FindDeviceCmd);
                     CommPort.Write(FindDeviceCmd);
-
                     waitPortRecvSignal.Reset();
+                    RecvPortData.Clear();
+                    currentDeviceAddr = "查询中...";
                     await Task.Run(() => 
                     {
-                        waitPortRecvSignal.Wait(5000);
+                        waitPortRecvSignal.Wait(1000);
 
                     });
-
-                    string RecvDataStr = "";
-                    RecvPortData.ForEach(x => { RecvDataStr += x; });
+                    string RecvDataStr = Encoding.ASCII.GetString(RecvPortData.ToArray());
                     Trace.WriteLine("接收到:" + RecvDataStr);
 
+                    if (RecvDataStr.StartsWith(MBData_Head) && RecvDataStr.Length > 6)
+                    {
+                        string RecvAddr = RecvDataStr.Substring(1, 2);
+                        string RecvFunction = RecvDataStr.Substring(3, 2);
+                        string RecvDataLen = RecvDataStr.Substring(5, 2);
+                        string RecvDataContent = RecvDataStr.Substring(7, int.Parse(RecvDataLen) * 2);
+                        Trace.WriteLine($"接收到设备响应:{RecvAddr},功能码:{RecvFunction},数据长度:{RecvDataLen},数据内容:{RecvDataContent}");
+                        if (RecvAddr == FindDeviceCmd.Substring(1, 2))
+                        {
+                            btn_FindDevice.IsEnabled = true;
+                            currentDeviceAddr = RecvAddr;
+                            return;
+                        }
+                    }
 
                 }
+                btn_FindDevice.IsEnabled = true;
             }
 
         }
---
+++ Soway传感器配置工具/ValuePage.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Soway传感器配置工具
+{
+    /// <summary>
+    /// ValuePage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ValuePage : Page
+    {
+        MainWindow? mainWindow = null;
+        string LogString = "";
+        Task GetValueTask;
+
+        public ValuePage()
+        {
+            InitializeComponent();
+
+            mainWindow = Application.Current.MainWindow as MainWindow;
+            if (mainWindow == null)
+            {
+                throw new Exception("Get MainWindow Error");
+            }
+        }
+
+        private void AddLog(string msg)
+        {
+            LogString += msg + "\n";
+            Trace.WriteLine(msg);
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(200);
+                    await Dispatcher.BeginInvoke(() =>
+                    {
+                        logdisplay.Text = LogString;
+                        if(LogString.Length > 10000)
+                        {
+                            LogString = "";
+                        }
+                        
+                    });
+                }
+
+            });
+        }
+
+        private bool GetValueTaskRunning = false;
+        private async void GetValueTaskFunc()
+        {
+            string DeviceCmd = mainWindow.currentDeviceAddr;
+            DeviceCmd += mainWindow.MBData_Functions[4];
+            DeviceCmd += mainWindow.currentDeviceAddr;
+            DeviceCmd += "00";
+            DeviceCmd += "0002";
+            string CompleteCmd = ModbusAsciiLrcHelper.GenerateCompleteAsciiCmdWithLrc(DeviceCmd);
+            while (GetValueTaskRunning)
+            {
+
+                try
+                {
+                    await Dispatcher.BeginInvoke(() =>
+                    {
+                        AddLog("----------------------------------------------------------------------------");
+                        AddLog("发送指令:" + CompleteCmd);
+                    });
+
+                    mainWindow.CommPort!.Write(CompleteCmd);
+                    mainWindow.waitPortRecvSignal.Reset();
+                    mainWindow.RecvPortData.Clear();
+                    await Task.Run(() =>
+                    {
+                        mainWindow.waitPortRecvSignal.Wait(1000);
+
+                    });
+                    string RecvDataStr = Encoding.ASCII.GetString(mainWindow.RecvPortData.ToArray());
+                    AddLog("接收到:" + RecvDataStr);
+                    if (RecvDataStr.Length > 15)
+                    {
+                        RecvDataStr = RecvDataStr.Trim('\r', '\n');
+                        string Value_HEX = RecvDataStr.Substring(11, 4);
+                        ushort Value_U16 = Convert.ToUInt16(Value_HEX, 16);
+
+                        mainWindow.DeviceADValue.Add(Value_U16);
+
+                        await Dispatcher.BeginInvoke(() =>
+                        {
+                            ValueList.Items.Add(Value_U16);
+                            tb_ADValueHex.Text = Value_HEX;
+                            tb_ADValue.Text = Value_U16.ToString();
+                        });
+
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    AddLog(ex.Message);
+                }
+                await Task.Delay(500);
+            }
+
+        }
+
+        private async void btn_StartGetValue_Click(object sender, RoutedEventArgs e)
+        {
+            if (mainWindow == null)
+            {
+                return;
+            }
+            if (mainWindow.CommPort == null)
+            {
+                return;
+            }
+            if (mainWindow.currentDeviceAddr == "请重新搜索" || mainWindow.currentDeviceAddr == "N/D")
+            {
+                MessageBox.Show("请先搜索设备获取当前地址！", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            if (mainWindow.CommPort!.IsOpen == true)
+            {       
+                if(btn_StartGetValue.Content.ToString() == "启动采集")
+                {
+                    btn_StartGetValue.Content = "停止采集";
+                    GetValueTaskRunning = true;
+                    GetValueTask = new Task(GetValueTaskFunc);
+                    GetValueTask.Start();
+                }
+                else
+                {
+                    btn_StartGetValue.Content = "启动采集";
+                    GetValueTaskRunning = false;
+                    GetValueTask.Dispose();
+
+                }
+                
+
+
+            }
+        }
+    }
+}
