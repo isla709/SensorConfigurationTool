e0d3b755f00475e9d19373c7bdce53500285396b
Nyarutoru ！ <Neruya709@outlook.com>
2026-01-06 20:55:57 +0800

    ReBuild and New Project Create

---
+++ 航伽液位变送器配置工具/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 航伽液位变送器配置工具
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
