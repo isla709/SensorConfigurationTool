b07fafbff0731ccd304e18bbbb3283a5e214ba1f
Nyarutoru ！ <Neruya709@outlook.com>
2026-01-06 20:50:59 +0800

    Rename

--- Soway传感器配置工具/Properties/Settings.Designer.cs
+++ Soway传感器配置工具/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Soway传感器配置工具.Properties {
+namespace Soway压力油位传感器配置工具.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
